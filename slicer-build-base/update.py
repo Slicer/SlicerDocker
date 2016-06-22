f51e2c66f46df54c3dd5552684bb6170cf63bce1
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2016-06-22 17:45:27 -0400

    ENH: slicer-build-base: Add script automating update of Dockerfile
    Reviewed-by: Max Smolens <max.smolens@kitware.com>

---
+++ slicer-build-base/update.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+import argparse
+import os
+import subprocess
+
+#
+# Usage:
+#
+#   $ ./update.py svn_revision svn_revision_date
+#
+# Example:
+#
+#   $ ./update.py 25199 2016-06-19
+#
+
+def update_dockerfile(dockerfile, svn_revison, svn_revision_date):
+    lines = []
+    with open(dockerfile) as f:
+        for line in f:
+            if 'ENV SLICER_VERSION' in line:
+                line = "ENV SLICER_VERSION %s\n" % svn_revison
+            if '# Slicer master ' in line:
+                line = "# Slicer master %s\n" % svn_revision_date
+            lines.append(line)
+    with open(dockerfile, 'w') as f:
+        f.writelines(lines)
+        print("File %s updated\n" % dockerfile)
+
+def _run(cmd):
+    print("Executing %s" % ' '.join(cmd))
+    subprocess.check_call(cmd)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Update "slicer-build-base/Dockerfile" and git commit.')
+    parser.add_argument("svn_revision")
+    parser.add_argument("svn_revision_date")
+    args = parser.parse_args()
+
+    slicer_build_base_dir = os.path.dirname(__file__)
+    dockerfile = os.path.join(slicer_build_base_dir, "Dockerfile")
+
+    update_dockerfile(dockerfile, args.svn_revision, args.svn_revision_date)
+
+    _run(['git', 'add', dockerfile])
+    message = 'ENH: slicer-build-base: Update to Slicer r%s from %s' % (args.svn_revision, args.svn_revision_date)
+    _run(['git', 'commit', '-m', message])
+
---
+++ slicer-build-base/update.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+#
+# Usage:
+#
+#  $ ./update.sh /path/to/src/Slicer
+#
+# Example:
+#
+#  $ ./update.sh ~/Projects/Slicer
+#  slicer_dir:/home/jcfr/Projects/Slicer
+#  svn_revision: 25199
+#  svn_revision_date: 2016-06-19
+#
+#  File /home/jcfr/Projects/SlicerDocker/slicer-build-base/Dockerfile updated
+#
+#  Executing git add /home/jcfr/Projects/SlicerDocker/slicer-build-base/Dockerfile
+#
+#  Executing git commit -m ENH: slicer-build-base: Update to Slicer r25199 from 2016-06-19
+#  [master 397eeab] ENH: slicer-build-base: Update to Slicer r25199 from 2016-06-19
+#   1 file changed, 2 insertions(+), 2 deletions(-)
+#
+
+script_dir="`cd $(dirname $0); pwd`"
+
+if test -z "$1"; then
+  echo "Usage: $0 /path/to/src/Slicer"
+  exit 1
+fi
+
+slicer_dir=$1
+
+echo "slicer_dir:$slicer_dir"
+
+pushd $slicer_dir > /dev/null 2>&1
+
+svn_revision=$(git svn info | grep "Last Changed Rev" | sed -e 's/Last Changed Rev: //g')
+echo "svn_revision: $svn_revision"
+
+svn_revision_date=$(git svn info | grep "Last Changed Date" | sed -e 's/Last Changed Date: //g' | cut -d" " -f1)
+echo "svn_revision_date: $svn_revision_date"
+
+popd > /dev/null 2>&1
+echo ""
+
+python $script_dir/update.py $svn_revision $svn_revision_date
+
