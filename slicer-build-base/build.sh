9435ad679daff6fe8b06be6433373fc411cbf036
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2016-09-27 23:44:27 -0400

    Associate build-time metadata with each image

--- slicer-build-base/build.sh
+++ slicer-build-base/build.sh
@@ -2,4 +2,11 @@
 
 script_dir="`cd $(dirname $0); pwd`"
 
-docker build -t slicer/slicer-build-base $script_dir
+image=slicer/slicer-build-base
+
+docker build -t $image \
+    --build-arg IMAGE=$image \
+    --build-arg VCS_REF=`git rev-parse --short HEAD` \
+    --build-arg VCS_URL=`git config --get remote.origin.url` \
+    --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
+    $script_dir
--- slicer-build/build.sh
+++ slicer-build/build.sh
@@ -2,4 +2,11 @@
 
 script_dir="`cd $(dirname $0); pwd`"
 
-docker build -t slicer/slicer-build $script_dir
+image=slicer/slicer-build
+
+docker build -t $image \
+    --build-arg IMAGE=$image \
+    --build-arg VCS_REF=`git rev-parse --short HEAD` \
+    --build-arg VCS_URL=`git config --get remote.origin.url` \
+    --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
+    $script_dir
