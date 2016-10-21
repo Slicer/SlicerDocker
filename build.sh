cb8db255be55aa810cc085568633848560205dff
Mayeul Chassagnard <mayeul.chassagnard@kitware.com>
2016-10-21 10:00:34 -0400

    STYLE: Rename slicer/slicer-build-deps TO slicer/slicer-dependecies

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 script_dir="`cd $(dirname $0); pwd`"
 
 $script_dir/slicer-build-base/build.sh && \
-$script_dir/slicer-build-deps/build.sh && \
+$script_dir/slicer-dependencies/build.sh && \
 $script_dir/slicer-build/build.sh
 
