33b17d6d3232e3f70bfde350cbc5fbadab080e1c
Mayeul Chassagnard <mayeul.chassagnard@kitware.com>
2016-10-21 14:54:16 -0400

    ENH: Use a new directory to build the Slicer tree including tests
    ENH: Add slicer/slicer-test in root directories: build/push/README

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ script_dir="`cd $(dirname $0); pwd`"
 
 $script_dir/slicer-build-base/build.sh && \
 $script_dir/slicer-dependencies/build.sh && \
-$script_dir/slicer-build/build.sh
+$script_dir/slicer-build/build.sh && \
+$script_dir/slicer-test/build.sh
 
