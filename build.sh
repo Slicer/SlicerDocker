5f47549ee530e8ad475f38ae70180f32acd5508e
Mayeul Chassagnard <mayeul.chassagnard@kitware.com>
2016-10-31 11:39:09 -0400

    ENH: Move slicer-test:opengl image from Slicer to SlicerDocker
    This image is intended to run the tests which needs X11 display
    
    It contains also the necessary files to configure the way these tests
    are performed and submitted to CDash

--- build.sh
+++ build.sh
@@ -5,5 +5,6 @@ script_dir="`cd $(dirname $0); pwd`"
 $script_dir/slicer-base/build.sh && \
 $script_dir/slicer-dependencies/build.sh && \
 $script_dir/slicer-build/build.sh && \
-$script_dir/slicer-test/build.sh
+$script_dir/slicer-test/build.sh && \
+$script_dir/slicer-test/opengl/build.sh
 
--- push.sh
+++ push.sh
@@ -5,4 +5,5 @@ script_dir="`cd $(dirname $0); pwd`"
 $script_dir/slicer-base/push.sh && \
 $script_dir/slicer-dependencies/push.sh && \
 $script_dir/slicer-build/push.sh && \
-$script_dir/slicer-test/push.sh
+$script_dir/slicer-test/push.sh && \
+$script_dir/slicer-test/opengl/push.sh
