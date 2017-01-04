576d58369293327156d686aaceb379612781a734
Mayeul Chassagnard <mayeul.chassagnard@kitware.com>
2017-01-04 14:54:38 -0500

    STYLE: Wrong Information
    * SITE_BUILD_TYPE removed temporally the time to find the good one which
      should come from the slicer-build docker image
    * SITE_PLATFORM changed to centOS 5

--- slicer-test/opengl/CircleCI_Slicer_Docker.cmake
+++ slicer-test/opengl/CircleCI_Slicer_Docker.cmake
@@ -39,27 +39,21 @@ set( Slicer_GIT_REPOSITORY "https://github.com/Slicer/Slicer.git" )
 set( CTEST_SITE "CircleCI_Slicer" )
 
 # Follow format for caps and components as given on Slicer dashboard
-set( SITE_PLATFORM "Ubuntu-64" )
-
-# Use SITE_BUILD_TYPE specified by circle.yml
-set( SITE_BUILD_TYPE "$ENV{SITE_BUILD_TYPE}" )
-if( NOT( (SITE_BUILD_TYPE MATCHES "Debug") OR (SITE_BUILD_TYPE MATCHES "Release") ) )
-  set( SITE_BUILD_TYPE "Debug" ) # Release, Debug
-endif( NOT( (SITE_BUILD_TYPE MATCHES "Debug") OR (SITE_BUILD_TYPE MATCHES "Release") ) )
+set( SITE_PLATFORM "CentOS5" )
 
 # Named SITE_BUILD_NAME
 string( SUBSTRING $ENV{CIRCLE_SHA1} 0 7 commit )
 set( what $ENV{CIRCLE_BRANCH} )
 set( SITE_BUILD_NAME_SUFFIX _${commit}_${what} )
 
-set( SITE_BUILD_NAME "CircleCI-${SITE_PLATFORM}-${SITE_BUILD_TYPE}${SITE_BUILD_NAME_SUFFIX}" )
+set( SITE_BUILD_NAME "CircleCI-${SITE_PLATFORM}-${SITE_BUILD_NAME_SUFFIX}" )
 
 set( CTEST_BUILD_NAME "${SITE_BUILD_NAME}-BuildTest-${SITE_CTEST_MODE}" )
 
 ######### Config ##########
 
-set( CTEST_CONFIGURATION_TYPE "${SITE_BUILD_TYPE}" )
-set( CMAKE_BUILD_TYPE "${SITE_BUILD_TYPE}" )
+#set( CTEST_CONFIGURATION_TYPE "${SITE_BUILD_TYPE}" )
+#set( CMAKE_BUILD_TYPE "${SITE_BUILD_TYPE}" )
 set( BUILD_TESTING ON )
 
 #set( BUILD_TOOL_FLAGS "-j8" )
