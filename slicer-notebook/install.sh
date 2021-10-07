6e89bbfebb275f794307e511283b64dbdf00cc0b
D G <dazzag@gmail.com>
2021-10-07 19:48:20 +0000

    Changes to allow both 4.11 and 4.13 builds. Plus temp workaround

--- slicer-notebook/install.sh
+++ slicer-notebook/install.sh
@@ -23,34 +23,41 @@ echo "XORG_PID [$XORG_PID]"
 ################################################################################
 # Set up Slicer extensions
 
-echo "1"
-
-# Set default application settings:
+echo "Set default application settings"
 # - use CPU volume rendering (it is better optimized for software rendering)
-$slicer_executable \
-  -c "slicer.app.settings().setValue('VolumeRendering/RenderingMethod','vtkMRMLCPURayCastVolumeRenderingDisplayNode'); \
-  slicer.app.settings().setValue('Markups/GlyphScale', 3); \
-  slicer.app.settings().setValue('Markups/UseGlyphScale', True)"
+$slicer_executable -c '
+slicer.app.settings().setValue("VolumeRendering/RenderingMethod","vtkMRMLCPURayCastVolumeRenderingDisplayNode")
+slicer.app.settings().setValue("Markups/GlyphScale", 3)
+slicer.app.settings().setValue("Markups/UseGlyphScale", True)
+'
 
-echo "2"
+echo "Install SlicerJupyter extension"
+$slicer_executable -c '
+em = slicer.app.extensionsManagerModel()
+extensionMetaData = em.retrieveExtensionMetadataByName("SlicerJupyter")
+print(f"extensionMetaData: {extensionMetaData}")
+if slicer.app.majorVersion*100+slicer.app.minorVersion < 413:
+    # Slicer-4.11
+    itemId = extensionMetaData["item_id"]
+    url = f"{em.serverUrl().toString()}/download?items={itemId}"
+    print(f"itemId: {itemId} url: {url}")
+    extensionPackageFilename = f"{slicer.app.temporaryPath}/{itemId}"
+    slicer.util.downloadFile(url, extensionPackageFilename)
+else:
+    # Slicer-4.13
+    #itemId = extensionMetaData["_id"]
+    itemId = "30304-linux-amd64-SlicerJupyter-gitb3fba2d-2021-03-21.tar.gz"
+    url = "https://github.com/Slicer/SlicerJupyter/files/7305919/30304-linux-amd64-SlicerJupyter-gitb3fba2d-2021-03-21.tar.gz"
+    print("Using bespoke build from {url} until https://github.com/Slicer/SlicerJupyter/issues/59 is fixed")
+    extensionPackageFilename = f"{slicer.app.temporaryPath}/{itemId}"
+    slicer.util.downloadFile(url, extensionPackageFilename)
+em.installExtension(extensionPackageFilename)
+'
 
-# Install SlicerJupyter extension
-$slicer_executable \
-  -c "em = slicer.app.extensionsManagerModel(); \
-  extensionMetaData = em.retrieveExtensionMetadataByName('SlicerJupyter'); \
-  print(extensionMetaData); \
-  itemId = extensionMetaData['item_id']; \
-  url = f\"{em.serverUrl().toString()}/download?items={itemId}\"; \
-  print(f\"itemId: {itemId} url: {url}\"); \
-  extensionPackageFilename = slicer.app.temporaryPath+'/'+itemId; \
-  slicer.util.downloadFile(url, extensionPackageFilename); \
-  em.installExtension(extensionPackageFilename)"
-
-echo "3"
-
-# Install Jupyter server (in Slicer's Python environment) and Slicer Jupyter kernel
-$slicer_executable \
-  -c "slicer.modules.jupyterkernel.installInternalJupyterServer()"
+echo "Install Jupyter server (in Slicer's Python environment) and Slicer Jupyter kernel"
+$slicer_executable -c '
+slicer.modules.jupyterkernel.installInternalJupyterServer()
+'
 
 ################################################################################
 # Shutdown headless environment
